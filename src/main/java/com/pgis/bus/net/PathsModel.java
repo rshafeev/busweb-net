fed86633a1649188f46a5310a8c794568773b7bc
Marianna <rm@premiumgis.com>
2013-01-19 19:34:16 +0200

    1-st commit

---
+++ src/main/java/com/pgis/bus/net/PathsModel.java
@@ -0,0 +1,24 @@
+package com.pgis.bus.net;
+
+public class PathsModel {
+
+	private int path_id;
+	private double cost;
+	public String toString(){
+		return "test2233";
+	}
+	
+	public int getPath_id() {
+		return path_id;
+	}
+	public void setPath_id(int path_id) {
+		this.path_id = path_id;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+}
